d6653ba712c7d7625753a46f57c6f7ab06272b40
lev5real <85600903+lev5real@users.noreply.github.com>
2022-12-04 21:57:43 +0300

    Add files via upload

---
+++ Utilities/G.cs
@@ -0,0 +1,16 @@
+﻿using elmencho.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace elmencho.Utilities
+{
+    public class G
+    {
+        public static List<Entity> EntityList = new List<Entity>();
+        public static Engine Engine;
+    }
+}
